08ee952131fab987c50af99f33737ea4d9994e5d
shreyuu <shreyashmeshram0031@gmail.com>
2023-09-06 21:33:03 +0530

    python webisite failed

--- main.py
+++ main.py
@@ -1,52 +1,53 @@
 # Import the necessary modules.
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mail import Mail, Message
 
 # Create a Flask app.
 app = Flask(__name__)
 
+# Configure Flask-Mail for sending emails.
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 'shreyashmeshram0031@gmail.com'  # Replace with your email
+app.config['MAIL_PASSWORD'] = 'Shreyash23@25'  # Replace with your email password
+
+mail = Mail(app)
+
 # Define the route for the contact page.
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    # If the user is submitting the form, process the data.
     if request.method == 'POST':
         name = request.form.get('name')
         email = request.form.get('email')
         message = request.form.get('message')
 
-        # Send the data to the server.
-        send_email(name, email, message)
+        if not name or not email or not message:
+            flash('All fields are required.', 'error')
+        else:
+            try:
+                send_email(name, email, message)
+                flash('Message sent successfully!', 'success')
+            except Exception as e:
+                flash(f'An error occurred: {str(e)}', 'error')
 
-        # Redirect the user to the thank-you page.
-        return render_template('thank_you.html')
+            return redirect(url_for('contact'))
 
-    # If the user is just viewing the page, show the form.
-    else:
-        return render_template('contact.html')
+    return render_template('contact.html')
 
-# Define the function to send the email.
 def send_email(name, email, message):
-    # Import the necessary modules.
-    import smtplib
-    from email.mime.multipart import MIMEMultipart
-    from email.mime.text import MIMEText
+    msg = Message('New Message from Portfolio Website',
+                  sender=email,
+                  recipients=['shreyashmeshram0031@gmail.com'])  # Replace with your email
 
-    # Create the email message.
-    msg = MIMEMultipart()
-    msg['Subject'] = 'New Message from Portfolio Website'
-    msg['From'] = email
-    msg['To'] = 'shreyashmeshram0031@gmail.com'
+    msg.body = message
 
-    # Create the body of the email.
-    body = MIMEText(message)
-    msg.attach(body)
+    try:
+        mail.send(msg)
+    except Exception as e:
+        raise Exception(f'Email sending failed: {str(e)}')
 
-    # Send the email.
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login('shreyashmeshram0031@gmail.com', 'your_password')
-    server.sendmail(email, 'shreyashmeshram0031@gmail.com', msg.as_string())
-    server.quit()
-
-# Run the app.
 if __name__ == '__main__':
-    app.run()
+    app.secret_key = 'shreyuu'  # Replace with a secure secret key
+    app.run(debug=True)
