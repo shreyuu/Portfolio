5b37ee6ba9ff6f22ca20fbdde412d44346158aee
Shreyash Meshram <91451934+shreyuu@users.noreply.github.com>
2025-03-27 22:33:55 +0530

    remove unused backend files and directories

--- backend/portfolio/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Project(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    technology = models.CharField(max_length=20)
-    github_link = models.URLField(max_length=200)
